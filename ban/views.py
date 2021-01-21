750c954c5ee6a6a5c972717904b86075e445adfb
ikalemmon <nagatadaiki@MacBook-Pro-7.local>
2021-01-21 14:23:29 +0900

    top

--- ban/views.py
+++ ban/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404,render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def top(request):
+    return render(request, 'ban/top.html')
